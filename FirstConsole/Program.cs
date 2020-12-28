6c9eccb10c2aec52579f90591f4556131650ae01
Agafitei Razvan <razvule@gmail.com>
2020-12-28 09:40:00 +0200

    Primul exercitiu

--- FirstConsole/Program.cs
+++ FirstConsole/Program.cs
@@ -9,10 +9,23 @@ namespace FirstConsole
 {
     class Program
     {
-        static void Main(string[] args)
+        static int NumarPrim(int numar)
         {
-            Console.WriteLine("Bun venit la curs, Razvan!");
-
+            if (numar < 1)
+                return 0;
+            int temp = numar - 1;
+           while(temp>1)
+            {
+                if (numar % temp == 0)
+                    return 0;
+                temp--;
+            } 
+            return 1;
+        }
+        static void Main()
+        {
+            //Console.WriteLine("Bun venit la curs, Razvan!");
+            Console.WriteLine("{0}", NumarPrim(18));
             Console.ReadKey();
         }
     }
