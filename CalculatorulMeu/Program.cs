dc1742b697c646a3367e7bdda9721a9d7507e9dd
Agafitei Razvan <razvule@gmail.com>
2021-01-01 13:38:35 +0200

    Add CalculatorulMeu

---
+++ CalculatorulMeu/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorulMeu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
