46a61bc2ac8f9c3ed743df5a31cf644d6ac32b36
Agafitei Razvan <razvule@gmail.com>
2021-01-11 18:15:14 +0200

    MyFIrstWinForm
    In mare parte merge bine, doar cu o singura exceptie. Cred c-o sa-ti dai seama

--- MyFirsrtWinForm/Form1.Designer.cs
+++ MyFirsrtWinForm/Form1.Designer.cs
@@ -29,40 +29,49 @@ namespace MyFirsrtWinForm
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox_name = new System.Windows.Forms.TextBox();
-            this.button_Send = new System.Windows.Forms.Button();
-            this.label_message = new System.Windows.Forms.Label();
+            this.textBox_Numar1 = new System.Windows.Forms.TextBox();
+            this.button_Calculeaza = new System.Windows.Forms.Button();
+            this.label_Scadere = new System.Windows.Forms.Label();
             this.button_Clear = new System.Windows.Forms.Button();
+            this.textBox_Numar2 = new System.Windows.Forms.TextBox();
+            this.label_Adunare = new System.Windows.Forms.Label();
+            this.label_Inmultire = new System.Windows.Forms.Label();
+            this.label_Impartire = new System.Windows.Forms.Label();
+            this.label_Check1 = new System.Windows.Forms.Label();
+            this.label_Check2 = new System.Windows.Forms.Label();
+            this.button_Verifica = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // textBox_name
+            // textBox_Numar1
             // 
-            this.textBox_name.Location = new System.Drawing.Point(174, 86);
-            this.textBox_name.Name = "textBox_name";
-            this.textBox_name.Size = new System.Drawing.Size(100, 20);
-            this.textBox_name.TabIndex = 0;
+            this.textBox_Numar1.Location = new System.Drawing.Point(32, 45);
+            this.textBox_Numar1.Name = "textBox_Numar1";
+            this.textBox_Numar1.Size = new System.Drawing.Size(100, 20);
+            this.textBox_Numar1.TabIndex = 0;
             // 
-            // button_Send
+            // button_Calculeaza
             // 
-            this.button_Send.Location = new System.Drawing.Point(347, 83);
-            this.button_Send.Name = "button_Send";
-            this.button_Send.Size = new System.Drawing.Size(75, 23);
-            this.button_Send.TabIndex = 1;
-            this.button_Send.Text = "Send";
-            this.button_Send.UseVisualStyleBackColor = true;
-            this.button_Send.Click += new System.EventHandler(this.button_Send_Click);
+            this.button_Calculeaza.Location = new System.Drawing.Point(292, 266);
+            this.button_Calculeaza.Name = "button_Calculeaza";
+            this.button_Calculeaza.Size = new System.Drawing.Size(75, 23);
+            this.button_Calculeaza.TabIndex = 1;
+            this.button_Calculeaza.Text = "Calculeaza";
+            this.button_Calculeaza.UseVisualStyleBackColor = true;
+            this.button_Calculeaza.Click += new System.EventHandler(this.button_Calculeaza_Click);
             // 
-            // label_message
+            // label_Scadere
             // 
-            this.label_message.AutoSize = true;
-            this.label_message.Location = new System.Drawing.Point(174, 164);
-            this.label_message.Name = "label_message";
-            this.label_message.Size = new System.Drawing.Size(0, 13);
-            this.label_message.TabIndex = 2;
+            this.label_Scadere.AutoSize = true;
+            this.label_Scadere.Location = new System.Drawing.Point(102, 244);
+            this.label_Scadere.Name = "label_Scadere";
+            this.label_Scadere.Size = new System.Drawing.Size(75, 13);
+            this.label_Scadere.TabIndex = 2;
+            this.label_Scadere.Text = "label_Scadere";
             // 
             // button_Clear
             // 
-            this.button_Clear.Location = new System.Drawing.Point(483, 82);
+            this.button_Clear.Location = new System.Drawing.Point(703, 415);
             this.button_Clear.Name = "button_Clear";
             this.button_Clear.Size = new System.Drawing.Size(75, 23);
             this.button_Clear.TabIndex = 3;
@@ -70,15 +79,92 @@ namespace MyFirsrtWinForm
             this.button_Clear.UseVisualStyleBackColor = true;
             this.button_Clear.Click += new System.EventHandler(this.button_Clear_Click);
             // 
+            // textBox_Numar2
+            // 
+            this.textBox_Numar2.Location = new System.Drawing.Point(267, 45);
+            this.textBox_Numar2.Name = "textBox_Numar2";
+            this.textBox_Numar2.Size = new System.Drawing.Size(100, 20);
+            this.textBox_Numar2.TabIndex = 4;
+            // 
+            // label_Adunare
+            // 
+            this.label_Adunare.AutoSize = true;
+            this.label_Adunare.Location = new System.Drawing.Point(102, 182);
+            this.label_Adunare.Name = "label_Adunare";
+            this.label_Adunare.Size = new System.Drawing.Size(75, 13);
+            this.label_Adunare.TabIndex = 5;
+            this.label_Adunare.Text = "label_Adunare";
+            // 
+            // label_Inmultire
+            // 
+            this.label_Inmultire.AutoSize = true;
+            this.label_Inmultire.Location = new System.Drawing.Point(102, 303);
+            this.label_Inmultire.Name = "label_Inmultire";
+            this.label_Inmultire.Size = new System.Drawing.Size(74, 13);
+            this.label_Inmultire.TabIndex = 6;
+            this.label_Inmultire.Text = "label_Inmultire";
+            // 
+            // label_Impartire
+            // 
+            this.label_Impartire.AutoSize = true;
+            this.label_Impartire.Location = new System.Drawing.Point(102, 364);
+            this.label_Impartire.Name = "label_Impartire";
+            this.label_Impartire.Size = new System.Drawing.Size(75, 13);
+            this.label_Impartire.TabIndex = 7;
+            this.label_Impartire.Text = "label_Impartire";
+            // 
+            // label_Check1
+            // 
+            this.label_Check1.AutoSize = true;
+            this.label_Check1.Location = new System.Drawing.Point(32, 97);
+            this.label_Check1.Name = "label_Check1";
+            this.label_Check1.Size = new System.Drawing.Size(72, 13);
+            this.label_Check1.TabIndex = 10;
+            this.label_Check1.Text = "label_Check1";
+            // 
+            // label_Check2
+            // 
+            this.label_Check2.AutoSize = true;
+            this.label_Check2.Location = new System.Drawing.Point(264, 97);
+            this.label_Check2.Name = "label_Check2";
+            this.label_Check2.Size = new System.Drawing.Size(72, 13);
+            this.label_Check2.TabIndex = 11;
+            this.label_Check2.Text = "label_Check2";
+            // 
+            // button_Verifica
+            // 
+            this.button_Verifica.Location = new System.Drawing.Point(450, 45);
+            this.button_Verifica.Name = "button_Verifica";
+            this.button_Verifica.Size = new System.Drawing.Size(75, 23);
+            this.button_Verifica.TabIndex = 12;
+            this.button_Verifica.Text = "Verifica";
+            this.button_Verifica.UseVisualStyleBackColor = true;
+            this.button_Verifica.Click += new System.EventHandler(this.button_Verifica_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(653, 137);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 8;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_Verifica);
+            this.Controls.Add(this.label_Check2);
+            this.Controls.Add(this.label_Check1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label_Impartire);
+            this.Controls.Add(this.label_Inmultire);
+            this.Controls.Add(this.label_Adunare);
+            this.Controls.Add(this.textBox_Numar2);
             this.Controls.Add(this.button_Clear);
-            this.Controls.Add(this.label_message);
-            this.Controls.Add(this.button_Send);
-            this.Controls.Add(this.textBox_name);
+            this.Controls.Add(this.label_Scadere);
+            this.Controls.Add(this.button_Calculeaza);
+            this.Controls.Add(this.textBox_Numar1);
             this.Name = "Form1";
             this.Text = "Andrei";
             this.ResumeLayout(false);
@@ -88,10 +174,18 @@ namespace MyFirsrtWinForm
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox_name;
-        private System.Windows.Forms.Button button_Send;
-        private System.Windows.Forms.Label label_message;
+        private System.Windows.Forms.TextBox textBox_Numar1;
+        private System.Windows.Forms.Button button_Calculeaza;
+        private System.Windows.Forms.Label label_Scadere;
         private System.Windows.Forms.Button button_Clear;
+        private System.Windows.Forms.TextBox textBox_Numar2;
+        private System.Windows.Forms.Label label_Adunare;
+        private System.Windows.Forms.Label label_Inmultire;
+        private System.Windows.Forms.Label label_Impartire;
+        private System.Windows.Forms.Label label_Check1;
+        private System.Windows.Forms.Label label_Check2;
+        private System.Windows.Forms.Button button_Verifica;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- MyFirsrtWinForm/Form1.cs
+++ MyFirsrtWinForm/Form1.cs
@@ -18,21 +18,106 @@ namespace MyFirsrtWinForm
            
         }
 
-
-        private void button_Send_Click(object sender, EventArgs e)
+        private void button_Calculeaza_Click(object sender, EventArgs e)
         {
-            label_message.Text = CiaoMessage(textBox_name.Text);
+            label_Adunare.Text = Adunare(Numar(textBox_Numar1.Text), Numar(textBox_Numar2.Text)).ToString();
+
+            label_Scadere.Text = Scadere(Numar(textBox_Numar1.Text), Numar(textBox_Numar2.Text)).ToString();
+
+            label_Inmultire.Text = Inmultire(Numar(textBox_Numar1.Text), Numar(textBox_Numar2.Text)).ToString();
+
+            label_Impartire.Text = Impartire(Numar(textBox_Numar1.Text), Numar(textBox_Numar2.Text)).ToString();
         }
 
-        private string CiaoMessage(string name)
+        private void button_Verifica_Click(object sender, EventArgs e)
         {
-            return $"Ciao {name}";
+            if (IsNumber(textBox_Numar1.Text))
+            {
+                label_Check1.Text = "Corect";
+            }
+            else
+            {
+                label_Check1.Text = "What the !@#$ is this? This ain't no number!";
+            }
+
+            if (IsNumber(textBox_Numar2.Text))
+            {
+                label_Check2.Text = "Corect";
+            }
+            else
+            {
+                label_Check2.Text = "What the !@#$ is this? This ain't no number!";
+            }
+        }
+
+        private static string Adunare(double numar1, double numar2)
+        {
+            var suma = numar1 + numar2;
+            return $"Suma este: {suma.ToString()}";
+        }
+
+        private static string Scadere(double numar1, double numar2)
+        {
+            var diferenta = numar1 - numar2;
+            return $"Diferenta este: {diferenta.ToString()}";
+        }
+
+        private static string Inmultire(double numar1, double numar2)
+        {
+            var produs = numar1 * numar2;
+            return $"Produsul este: {produs.ToString()}";
+        }
+
+        private static string Impartire(double numar1, double numar2)
+        {
+            if(numar2==0)
+            {
+                return "Nu poti imparti la 0";
+            }
+            var cat = numar1 / numar2;
+            return $"Catul este: {cat.ToString()}";
         }
 
         private void button_Clear_Click(object sender, EventArgs e)
         {
-            label_message.Text = string.Empty;
-            textBox_name.Text = string.Empty;
+            textBox_Numar1.Text = string.Empty;
+            textBox_Numar2.Text = string.Empty;
+            label_Adunare.Text = string.Empty;
+            label_Scadere.Text = string.Empty;
+            label_Inmultire.Text = string.Empty;
+            label_Impartire.Text = string.Empty;
+            label_Check1.Text = string.Empty;
+            label_Check2.Text = string.Empty;
+        }
+
+        private bool IsNumber(string cuvant)
+        {
+            int count = 0;
+            string temp = cuvant.Replace(',', '.');
+
+            for (int i = 0; i < temp.Length; i++)
+            {
+                var x = temp.Substring(i, 1);
+
+                if (x == ".")
+                {
+                    count++;
+                }
+            }
+
+            if (count > 1)
+            {
+                return false;
+            }
+
+            var result = double.TryParse(cuvant, out double corect);
+            return result;
+        }
+
+        private double Numar(string cuvant)
+        {
+            var corect = double.TryParse(cuvant, out double numar);
+            return numar;
         }
     }
 }
